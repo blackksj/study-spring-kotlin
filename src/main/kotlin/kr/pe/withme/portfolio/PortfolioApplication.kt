2680a316dd6578c8bedbebd419ccb049f2f0525a
darkksj <blackksj@nate.com>
2024-04-21 20:31:01 +0900

    Git과 Github

---
+++ src/main/kotlin/kr/pe/withme/portfolio/PortfolioApplication.kt
@@ -0,0 +1,11 @@
+package kr.pe.withme.portfolio
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PortfolioApplication
+
+fun main(args: Array<String>) {
+	runApplication<PortfolioApplication>(*args)
+}
